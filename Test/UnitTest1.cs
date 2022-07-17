daaeb06186b59bba1b9d51470ed2ff6554348ee6
jssson <johannesholzer@web.de>
2022-07-17 22:16:25 +0200

    accepted

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -3,13 +3,54 @@ using Zolution;
 
 public class UnitTest1
 {
-    [Fact]
-    public void GivenStrings_WhenNoCommon_ThenReturnEmptyString()
+    [Theory]
+    [InlineData("reflower","flow","flight")]
+    public void GivenStrings_WhenNoCommonStart_ThenReturnEmptyString(params string[] strs)
     {
         // Arrange
         Solution solution = new Solution();
-        string[] strs = {"dog","racecar","car"};
+        
         string result = "";
+        // Act
+    
+        // Assert
+        Assert.Equal(solution.LongestCommonPrefix(strs),result);
+    }  
+
+    [Theory]
+    [InlineData("dog","racecar","car")]
+    public void GivenStrings_WhenNoCommon_ThenReturnEmptyString(params string[] strs)
+    {
+        // Arrange
+        Solution solution = new Solution();
+        
+        string result = "";
+        // Act
+    
+        // Assert
+        Assert.Equal(solution.LongestCommonPrefix(strs),result);
+    }    
+    
+    [Fact]
+    public void GivenString_WhenNoCommon_ThenReturnString()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        string[] strs = {"a"};
+        string result = "a";
+        // Act
+    
+        // Assert
+        Assert.Equal(solution.LongestCommonPrefix(strs),result);
+    }    
+
+    [Fact]
+    public void GivenStrings_WhenCommon_ThenReturnCommonPart()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        string[] strs = {"flower","flow","flight"};
+        string result = "fl";
         // Act
     
         // Assert
--- Zolution/Class1.cs
+++ Zolution/Class1.cs
@@ -1,6 +1,41 @@
 ﻿namespace Zolution;
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        throw new NotImplementedException();
+        string result = "";
+        if(strs.Count()==1){
+            return strs[0];
+        }
+        for (int no = 0; no < strs.Count()-1; no++)
+        {
+            var str1=strs[no].ToArray();
+            var str2=strs[no+1].ToArray();
+            if (no == 0)
+            {
+                result = NewMethod(result, str1, str2);
+            }
+            else
+            {
+                result = NewMethod("", result.ToArray(), str2);
+            }
+        }
+        
+        return result;
+    }
+
+    private static string NewMethod(string result, char[] str1, char[] str2)
+    {
+        for (int nos = 0; nos < str1.Count() && nos < str2.Count(); nos++)
+        {
+            if (str1[nos] == str2[nos])
+            {
+                result += str1[nos];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return result;
     }
 }
